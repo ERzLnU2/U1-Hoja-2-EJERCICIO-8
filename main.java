1f69686728412d0b2ab4765adf315f9602b46591
solomongo™ <80227002+solomongoGUI@users.noreply.github.com>
2022-09-26 22:16:06 +0200

    Update main.java

--- main.java
+++ main.java
@@ -5,25 +5,19 @@
    🔴 Programa que pide por teclado el radio de un círculo, 
       y visualiza luego su longitud y su área.
       
-   🔴 Utiliza la clase scanner y Math y y los valores pueden contener decimales.
+   🔴 Utiliza la Clase Scanner y Math y y los valores pueden contener decimales.
 */
 package com.solomongo.ejercicio8_hoja2;
-import java.util.Scanner;
-
-
+import java.util.Scanner;  
  public class Ejercicio8_Hoja2 {
   public static void main(String[] args) {
-
-  Scanner sc = new Scanner(System.in);
-        double radio, volumen;
+  Scanner sc=new Scanner(System.in); // la Clase Scanner(MAY) pide datos por pantalla.
+        double radio, longitud, area;  // declarar variables con double, posiaca..
         System.out.println("\nEscribe el radio del circulo: ");
-        radio = sc.nextDouble();      
-        System.out.println("Longitud del radio del circulo: "+(radio*2)); 
-   
-        /* ///////////////////////////////////////
-                     process.. 45% 🏴‍
-        /////////////////////////////////////// */
-        
-        System.out.println("\n\nArea del radio del circulo: "+radio+"="+ (4.0/3)* Math.PI * Math.pow(radio, 3)+"\n"); 
-    }
+        radio=sc.nextDouble();      
+        System.out.println("Longitud del radio del circulo: "+(radio*2)); // aqui facil aplica la formula
+        area= Math.PI*Math.pow(radio,2);  // el calculo de laformula, con metodo POW de Clase MATH..*****
+        System.out.println("\nArea del radio del circulo: "+area);
+ }
 }
+
